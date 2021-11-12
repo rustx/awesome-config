5630c83098129b7c829436d0ab12c005db37b8a9
rustx <rustx@teknicity.net>
2021-11-13 00:01:05 +0100

    use helpers to get network interface

--- rc.lua
+++ rc.lua
@@ -29,9 +29,10 @@ Apps = {
 }
 
 -- network interfaces
+local ifaces = helpers.get_active_ifaces()
 Network_Interfaces = {
-  wlan = "wlp58s0",
-  lan = "enp42s0",
+  wlan = string.match(table.concat(ifaces, " "), "^wl[%l%d+]+" ),
+  lan  = string.match(table.concat(ifaces, " "), "^en[%l%d+]+" ),
 }
 
 -- language mappings
